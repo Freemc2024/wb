ab4ef3992e1baf912d05861b9d40a400426b855e
Freemc2024 <free2024mc@gmail.com>
2026-07-10 07:10:50 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "rjREHZpB#qioIMoPy9Uy3pYtUJeFqQM7BADneAkeLwS3YXQKZklQ",
+ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 PASIYA-WhatsappBot Is Alive Now😍*",
+BOT_OWNER: '94772582943',  // Replace with the owner's phone number
+
+
+
+};
